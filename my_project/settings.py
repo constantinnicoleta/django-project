e9e1c31680480244867486ea4cd9ce193f2a3e3d
Nicoleta Constantin <constantinnicoleta@hotmail.com>
2024-09-03 23:20:23 +0000

    add new files and proc file

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%c(q*%x-!5bf2rajsq#bv1d-r$(&+&*ou%q2lg$eu(nja3w0&9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-constantinn-djangoproje-m03cbe7uh0c.ws-eu115.gitpod.io']
+ALLOWED_HOSTS = ['8000-constantinn-djangoproje-m03cbe7uh0c.ws-eu115.gitpod.io','.herokuapp.com']
 
 
 # Application definition
